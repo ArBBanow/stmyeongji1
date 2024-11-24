1519ec43564544504bbd99d108c3f3b1ccc9a19b
ArBBanow <songjihoonteacher@naver.com>
2024-11-24 22:18:24 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import dice
+import updown
+import df
 
-st.title("연습하는 중")
-st.header("streamlit ex")
+st.sidebar.title("메뉴선택")
+page = st.sidebar.radio("", ["주사위던지기", "업다운게임", "데이터분석"])
+
+if page == "주사위던지기":
+  st.title("주사위던지기")
+  dice.show()
+
+elif page == "업다운게임":
+  st.title("업다운게임")
+  updown.show()
+
+# elif page == "데이터분석":
+#   st.title("데이터분석")
+#   df.show()
